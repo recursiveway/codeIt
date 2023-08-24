ff2468fe8e41288282b07b508631dd6e2777eb74
Ashutosh Choudhary <ashutoshchoudhary998@gmail.com>
2023-08-24 23:03:49 +0530

    hello world

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -17,33 +17,11 @@ import ThemeDropdown from "./ThemeDropdown";
 import LanguagesDropdown from "./LanguagesDropdown";
 
 const javascriptDefault = `/**
-* Problem: Binary Search: Search a sorted array for a target value.
+* Say Hello and start Coding coz Hello World is the most important thing out there.
 */
 
-// Time: O(log n)
-const binarySearch = (arr, target) => {
- return binarySearchHelper(arr, target, 0, arr.length - 1);
-};
 
-const binarySearchHelper = (arr, target, start, end) => {
- if (start > end) {
-   return false;
- }
- let mid = Math.floor((start + end) / 2);
- if (arr[mid] === target) {
-   return mid;
- }
- if (arr[mid] < target) {
-   return binarySearchHelper(arr, target, mid + 1, end);
- }
- if (arr[mid] > target) {
-   return binarySearchHelper(arr, target, start, mid - 1);
- }
-};
-
-const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const target = 5;
-console.log(binarySearch(arr, target));
+console.log(Hello World!!!);
 `;
 
 const Landing = () => {
